96a084e28d3ec215f2565c438a9e383ddc465bbd
fernandofs21 <fernando.silva@sptech.school>
2025-10-10 12:02:56 -0300

    fix: ajustes de criação de pastas

--- Infra AWS/ec2.tf
+++ Infra AWS/ec2.tf
@@ -9,7 +9,10 @@ resource "aws_instance" "web1" {
 
   user_data = join("\n\n", [
         file("${path.module}/scripts/instalar_docker_ubuntu.sh"),
-        file("${path.module}/scripts/instalar_nginx.sh")
+        file("${path.module}/scripts/instalar_nginx.sh"),
+        "mkdir -p /home/ubuntu/backend",
+        "sudo chown -R ubuntu:ubuntu /home/ubuntu/backend",
+        "sudo chmod 755 /home/ubuntu/backend"
     ])
 
   user_data_replace_on_change = true
@@ -49,7 +52,7 @@ resource "aws_instance" "db1" {
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.private1.id
   vpc_security_group_ids = [aws_security_group.db_sg.id]
-  associate_public_ip_address = true
+  # associate_public_ip_address = true
   private_ip             = "10.0.0.20"
 
   user_data = join("\n\n", [
@@ -68,7 +71,7 @@ resource "aws_instance" "db2" {
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.private2.id
   vpc_security_group_ids = [aws_security_group.db_sg.id]
-  associate_public_ip_address = true
+  # associate_public_ip_address = true
   private_ip             = "10.0.0.52"
   tags                   = { Name = "DB2" }
 }
--- Infra AWS/scripts/instalar_java.sh
+++ Infra AWS/scripts/instalar_java.sh
@@ -11,5 +11,5 @@ echo "Diretório /usr/share/api criado com sucesso."
 # Decodifica e salva o YAML
 echo "${arquivo_docker_compose}" | base64 -d > /home/ubuntu/compose.yaml
 
-sudo docker compose -f /home/ubuntu/compose.yaml up -d
+sudo docker-compose -f /home/ubuntu/compose.yaml up -d
 echo "API iniciada com sucesso."
